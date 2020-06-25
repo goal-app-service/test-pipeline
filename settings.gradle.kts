54bf35c9dbfe8ace05e0822f03376c4c2e565a5e
goal-app-service <goalappservice@gmail.com>
2020-06-25 21:43:06 +0300

    first commit

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,11 +1,15 @@
 rootProject.name = "test-pipeline"
 
 pluginManagement {
+    val springBootPluginVersion: String by settings
+    val springDependencyManagementPluginVersion: String by settings
     val dockerRemoteApiVersion: String by settings
 
     resolutionStrategy {
         eachPlugin {
             when (requested.id.id) {
+                "org.springframework.boot" -> useVersion(springBootPluginVersion)
+                "io.spring.dependency-management" -> useVersion(springDependencyManagementPluginVersion)
                 "com.bmuschko.docker-remote-api" -> useVersion(dockerRemoteApiVersion)
             }
         }
