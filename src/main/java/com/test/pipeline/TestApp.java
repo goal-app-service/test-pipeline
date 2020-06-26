79885f0bd420ee0cc66aade32705c516755bc05a
goal-app-service <goalappservice@gmail.com>
2020-06-26 19:57:21 +0300

    first commit

--- src/main/java/com/test/pipeline/TestApp.java
+++ src/main/java/com/test/pipeline/TestApp.java
@@ -1,5 +1,6 @@
 package com.test.pipeline;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @SpringBootApplication
 public class TestApp {
+    public static void main(String[] args) {
+        SpringApplication.run(TestApp.class, args);
+    }
 
     @RequestMapping("/")
     public String index() {
